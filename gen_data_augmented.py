aee877b8f6f3693d21c22d25793853d470936c9f
root <root@DESKTOP-3QKUKJ9.localdomain>
2021-04-01 23:10:45 +0900

    update

--- gen_data_augmented.py
+++ gen_data_augmented.py
@@ -1,57 +1,54 @@
 from PIL import Image
-import os, glob
+import os
+from glob import glob
 import numpy as np
-from sklearn import model_selection
-
-classes = ["monkey","boar","crow"]
-num_classes = len(classes)
-image_size_h = 50
-image_size_w = 50
-num_testdata = 100
-
-#画像読み込み
-
-X_train = []
-X_test = []
-Y_train = []
-Y_test = []
-
-for index, classlabel in enumerate(classes):
-    photos_dir = "./" + classlabel
-    files = glob.glob(photos_dir + "/*.jpg")
-    for i, file in enumerate(files):
-        if i >= 195: break
-        image = Image.open(file)
-        image = image.convert("RGB")
-        image = image.resize((image_size_w, image_size_h))
-        data = np.asarray(image)
-
-        if i < num_testdata:
-            X_test.append(data)
-            Y_test.append(index)
-        else:
-            for angle in range(-20,20,5):
-                # 回転
-                img_r = image.rotate(angle)
-                data = np.asarray(img_r)
-                X_train.append(data)
-                Y_train.append(index)
-
-                # 反転
-                img_trans = img_r.transpose(Image.FLIP_LEFT_RIGHT)
-                data = np.asarray(img_trans)
-                X_train.append(data)
-                Y_train.append(index)
 
 
-#X = np.array(X)
-#Y = np.array(Y)
+def data_aug():
+    src_root_dir = "/mnt/c/Users/yishi/Desktop/work/input/jscas"
+    save_root_dir = "/mnt/c/Users/yishi/Desktop/work/result/Images/jscas"
 
-X_train = np.array(X_train)
-X_test = np.array(X_test)
-y_train = np.array(Y_train)
-y_test = np.array(Y_test)
+    src_dirs = glob( os.path.join( src_root_dir, "*") )
 
-#X_train, X_test, Y_train, Y_test = model_selection.train_test_split(X, Y)
-xy = (X_train, X_test, y_train, y_test)
-np.save("./animal_aug.npy", xy)
+    for src_dir in src_dirs:
+        labels = glob(
+            os.path.join(src_dir, "label/*.png")
+        )
+        for label_path in labels:
+            img_path = label_path.replace("label", "movieframe")
+            _augment(label_path, src_root_dir, save_root_dir)
+            _augment(img_path, src_root_dir, save_root_dir)
+
+
+def _augment(img_path, src_root_dir, save_root_dir):
+    image = Image.open(img_path)
+    image = image.convert("RGB")
+    save_path = img_path.replace(src_root_dir, save_root_dir)
+    save_dir = os.path.dirname(save_path)
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+
+    for i, angle in enumerate(range(-45,46,45)):
+        # 回転
+        img_r = image.rotate(angle)
+        save_r_path = _change_filename(save_path, f"r{i}")
+        img_r.save(save_r_path)
+
+        # 反転
+        img_trans = img_r.transpose(Image.FLIP_LEFT_RIGHT)
+        save_t_path = _change_filename(save_path, f"t{i}")
+        img_trans.save(save_t_path)
+
+
+def _change_filename(path, addition_txt):
+    basename = os.path.basename(path)
+    s_basename = basename.split(".")
+    new_name = s_basename[0] + addition_txt
+    out_path = path.replace(
+        s_basename[0],
+        new_name
+    )
+    return out_path
+
+if __name__ == "__main__":
+    data_aug()
